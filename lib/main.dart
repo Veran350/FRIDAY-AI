fd13b49f9f28d522767a5bc61daeeb5ccc1eff4e
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-06-30 13:06:40 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(const FridayAI());
+}
+
+class FridayAI extends StatelessWidget {
+  const FridayAI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FRIDAY AI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
